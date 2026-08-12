6b2f20be5d53d4f7881e920e9f3eb417ca4c858b
Sumit Kumar Jha <88138799+hlosumitjha@users.noreply.github.com>
2026-08-12 11:56:22 +0530

    Use environment variable for server port

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ import { User, Note, Folder, Comment, Notification, Classroom, UserRole, Report 
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '20mb' }));
 
